aa6902ee9db4c4189d28bb60b76ec510403ba5f1
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-09-24 18:03:15 +0200

    Created invoices-list button and common styles.

---
+++ src/common/InvoicesListBtn/InvoicesListBtn.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+
+import '../CommonStyles.css'
+
+export const InvoicesListBtn = () => {
+    return (
+        <Link to='/invoices-list' className="menu-btn">
+            <div className="icon-background">
+                <FormatListBulletedIcon className="menu-btn-icon"/>
+            </div>
+            <div>
+                Twoje faktury
+            </div>
+        </Link>
+    )
+}
--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import {InvoicesListBtn} from "../../common/InvoicesListBtn/InvoicesListBtn";
 
 export const Main = () => {
     return(
-        <div>Strona główna</div>
+        <div>
+            Strona główna
+            <InvoicesListBtn/>
+        </div>
     )
 }
