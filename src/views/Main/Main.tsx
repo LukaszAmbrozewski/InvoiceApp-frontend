3749cbd06940efa24203f998377153fe2e5d08b1
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-09-25 19:05:08 +0200

    Moved menu buttons to separate component.

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,39 +1,14 @@
 import React from 'react';
-import {InvoicesListBtn} from "../../components/InvoicesListBtn/InvoicesListBtn";
-
-import './Main.css'
-import {AddClientBtn} from "../../components/AddClientBtn/AddClientBtn";
-import {ClientsListBtn} from "../../components/ClientsListBtn/ClientsListBtn";
-import {PatchUserBtn} from "../../components/PatchUserBtn/PatchUserBtn";
-import {HistoryBtn} from "../../components/HistoryBtn/HistoryBtn";
-import {StatisticsBtn} from "../../components/StatisticsBtn/StatisticsBtn";
 import {HeaderMobile} from "../../components/HeaderMobile/HeaderMobile";
 import {Footer} from "../../components/Footer/Footer";
+import {MainMenuBox} from "../../components/MainMenuBox/MainMenuBox";
+import './Main.css'
 
 export const Main = () => {
-    return(
+    return (
         <nav className="main">
             <HeaderMobile mainHeaderText='Strona główna' secondHeaderText='Zarządzaj swoimi fakturami'/>
-            <div className="menu-box">
-                <div>
-                    <InvoicesListBtn/>
-                </div>
-                <div>
-                    <AddClientBtn/>
-                </div>
-                <div>
-                    <ClientsListBtn/>
-                </div>
-                <div>
-                    <PatchUserBtn/>
-                </div>
-                <div>
-                    <HistoryBtn/>
-                </div>
-                <div>
-                    <StatisticsBtn/>
-                </div>
-            </div>
+            <MainMenuBox/>
             <Footer/>
         </nav>
     )
