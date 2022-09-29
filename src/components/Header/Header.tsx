b05f2d8397f86e3087f83842d0a09db5dbf08696
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-09-29 21:40:51 +0200

    Configured header for big screens.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
-
-import './Header.css'
 import {ShowPersonMenuBtn} from "../ShowPersonMenuBtn/ShowPersonMenuBtn";
+import {isMobile} from "react-device-detect";
+import './Header.css'
+import {Link} from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import AddIcon from "@mui/icons-material/Add";
+import InfoIcon from "@mui/icons-material/Info";
 
-interface Props{
+interface Props {
     mainHeaderText: string;
 }
 
 export const Header = (props: Props) => {
-    return(
-        <div className="header-mobile-box">
-            <h1 className='mobile-main-header'>
-                <div>{props.mainHeaderText}</div>
-                <ShowPersonMenuBtn/>
-            </h1>
-
-        </div>
+    return (
+        <>
+            {
+                isMobile ? (
+                    <div className="header-box">
+                        <h1 className='main-header, main-header-mobile'>
+                            <div>{props.mainHeaderText}</div>
+                            <ShowPersonMenuBtn/>
+                        </h1>
+                    </div>
+                ) : (
+                    <div className="header-box">
+                        <h1 className='main-header'>
+                            <div className="header-icon-box">
+                                <Link to='/main' className="header-btn"><HomeIcon className="header-icon"/></Link>
+                                <Link to='/info' className="header-btn"><InfoIcon className="header-icon"/></Link>
+                                <Link to='/invoice-add' className="header-btn"><AddIcon className="header-add-icon"/></Link>
+                            </div>
+                            <div className="header-element">{props.mainHeaderText}</div>
+                            <div className="header-element, header-profile-icon">
+                                <ShowPersonMenuBtn/>
+                            </div>
+                        </h1>
+                    </div>
+                )
+            }
+        </>
     )
 }
