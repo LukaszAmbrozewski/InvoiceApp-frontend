1cf78af8a9a6af60d61d82eaa52d5edfcd60f925
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-03 12:20:40 +0200

    Added common button.

---
+++ src/common/MainBtn/MainBtn.tsx
@@ -0,0 +1,22 @@
+import React, {MouseEventHandler} from 'react';
+import {Link} from "react-router-dom";
+
+import './MainBtn.css'
+
+interface Props {
+    text: string;
+    to?: string;
+    onClick?: MouseEventHandler;
+}
+
+export const MainBtn = (props: Props) => {
+    return (
+        props.to
+            ? <div className="main-btn-box">
+                <Link className="main-btn" onClick={props.onClick} to={props.to}>{props.text}</Link>
+            </div>
+            : <div className="main-btn-box">
+                <button className="main-btn" onClick={props.onClick}>{props.text}</button>
+            </div>
+    )
+}
