caec9981cb7751dbcc31ce7ddf133bcf2f4102b2
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-04 16:24:19 +0200

    Refactored styles.

--- src/components/UserAddFailedBox/UserAddFailedBox.tsx
+++ src/components/UserAddFailedBox/UserAddFailedBox.tsx
@@ -5,9 +5,9 @@ import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 export const UserAddFailedBox = () => {
     return (
-        <div className='user-added-box'>
-            <ErrorOutlineIcon className='user-added-icon'/>
-            <p className='user-added-text'>Podany adres email jest już zajęty. Spróbuj ponownie wykorzystując inny adres email.</p>
+        <div className='main-info-box'>
+            <ErrorOutlineIcon className='main-info-icon'/>
+            <p className='main-info-text'>Podany adres email jest już zajęty. Spróbuj ponownie wykorzystując inny adres email.</p>
             <MainBtn to='/register' text='Spróbuj ponownie'></MainBtn>
         </div>
     )
--- src/components/UserAddedBox/UserAddedBox.tsx
+++ src/components/UserAddedBox/UserAddedBox.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {MainBtn} from "../../common/MainBtn/MainBtn";
-import './UserAddedBox.css'
+import '../ComponentsStyles.css'
 import HowToRegIcon from '@mui/icons-material/HowToReg';
 
 export const UserAddedBox = () => {
     return (
-        <div className='user-added-box'>
-            <HowToRegIcon className='user-added-icon'/>
-            <p className='user-added-text'>Rejestracja przebiegła pomyślnie. Zaloguj się by móc korzystać z aplikacji.</p>
+        <div className='main-info-box'>
+            <HowToRegIcon className='main-info-icon'/>
+            <p className='main-info-text'>Rejestracja przebiegła pomyślnie. Zaloguj się by móc korzystać z aplikacji.</p>
             <MainBtn to='/login' text='Zaloguj się'></MainBtn>
         </div>
     )
