8a4f41574a220c51379a620035d1c242e825b538
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-08 14:27:47 +0200

    Configured user patch view and patch function.

--- src/views/UserPatch/UserPatch.tsx
+++ src/views/UserPatch/UserPatch.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {Header} from "../../components/Header/Header";
 import {SecondHeader} from "../../components/SecondHeader/SecondHeader";
 import {Footer} from "../../components/Footer/Footer";
+import {UserPatchBox} from "../../components/UserPatchBox/UserPatchBox";
 
 export const UserPatch = () => {
     return(
         <nav className="views">
             <Header mainHeaderText='Edytuj dane'/>
             <SecondHeader secondHeaderText='Edytuj dane swojej firmy'></SecondHeader>
-            <div>Formularz edycji danych firmy</div>
+            <UserPatchBox/>
             <Footer/>
         </nav>
     )
