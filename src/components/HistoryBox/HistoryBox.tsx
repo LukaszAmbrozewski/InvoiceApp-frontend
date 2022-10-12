d06b1e8fe074803634eec587d117cf8e7703e18e
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-12 09:19:21 +0200

    Created user action history view.

---
+++ src/components/HistoryBox/HistoryBox.tsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import '../ComponentsStyles.css'
+import {isMobile} from "react-device-detect";
+import {OneAction} from "types";
+import {Loading} from "../../common/Loading/Loading";
+import {getUserHistory} from "../../utils/getUserHistory";
+import {OneHistoryAction} from "../OneHistoryAction/OneHistoryAction";
+
+export const HistoryBox = () => {
+    const [history, setHistory] = useState<OneAction[] | undefined>()
+
+    const fetchUserHistory = async () => {
+        setHistory(await getUserHistory())
+    }
+
+    useEffect(() => {
+        fetchUserHistory()
+    })
+
+
+return (
+    <div className={isMobile ? 'data-box-mobile' : 'data-box'}>
+        {history ? (
+            history.map(history => (
+                <OneHistoryAction action={history.action} date={history.date} time={history.time} key={history.id}/>
+            ))
+        ) : (
+            <Loading/>
+        )}
+    </div>
+)
+}
+
+// // Zmniejsz odstęp pomiedzy linijkami data i godzina
