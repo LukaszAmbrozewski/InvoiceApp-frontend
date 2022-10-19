129659f4954739562a5ba78e480fcd387a3262c2
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-19 12:15:49 +0200

    Created client select component.

---
+++ src/components/ClientSelect/ClientSelect.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Client} from "types";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
+import './ClientSelect.css'
+import '../ComponentsStyles.css';
+
+interface Props {
+    selectClient: (clientId: string) => void;
+    clientsList: Client[];
+}
+
+export const ClientSelect = (props: Props) => {
+    const handleChange = (event: SelectChangeEvent) => {
+        props.selectClient(event.target.value);
+    };
+
+    return (
+        <FormControl
+            className='select-client-box'
+            variant="filled"
+            sx={{
+                div: {color: '#FFFFFF', '&:after': {border: 0}},
+                label: {color: '#696868', fontWeight: 'bold'},
+                svg: {color: '#FFFFFF'}
+            }}
+        >
+            <InputLabel id="demo-simple-select-filled-label">Wybierz kontrahenta z listy</InputLabel>
+            <Select
+                labelId="demo-simple-select-filled-label"
+                id="demo-simple-select-filled"
+                onChange={handleChange}
+                className='select-client-option'
+            >
+                {props.clientsList.map(client => (
+                        <MenuItem value={client.id} key={client.id}>{client.companyName}</MenuItem>
+                    )
+                )}
+            </Select>
+        </FormControl>
+    )
+}
+
