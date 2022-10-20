e524f1eee564309d65592b6da2f471051a6e5083
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-20 11:39:50 +0200

    Added create invoice view.

---
+++ src/components/InvoiceAddForm/InvoiceAddForm.tsx
@@ -0,0 +1,128 @@
+import React, {useState} from 'react';
+import {Field, Form, Formik} from "formik";
+import {StyledTextField} from "../../common/StyledTextField/StyledTextField";
+import {ConfirmBtn} from "../../common/ConfirmBtn/ConfirmBtn";
+import {InvoiceAdd} from "types";
+import '../ComponentsStyles.css'
+import {schemaAddInvoice} from "../../Validations/InvoiceValidation";
+import {apiUrl} from "../../config/api";
+import {useNavigate} from "react-router-dom";
+import './InvoiceAddForm.css'
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+
+interface Props{
+    buttonText: string,
+    clientId: string;
+}
+
+const now = new Date();
+const fullDate = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
+
+const initialValues = {
+    place: "",
+    creationDate: fullDate,
+    dateOfService: fullDate,
+    personCreatingInvoice: "",
+};
+
+export const InvoiceAddFrom = (props: Props) => {
+    const [clientAlert, setClientAlert] = useState<boolean>(false)
+    const navigate = useNavigate();
+
+    const addInvoice = async (values: InvoiceAdd) => {
+        if(props.clientId === ''){
+            setClientAlert(true)
+        } else {
+            const res = await fetch(`${apiUrl}/invoices`, {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    clientId: props.clientId,
+                    creationDate: values.creationDate,
+                    dateOfService: values.dateOfService,
+                    place: values.place,
+                    personCreatingInvoice: values.personCreatingInvoice,
+                }),
+            });
+
+            const response = await res.json();
+            response.isSuccess ? navigate('/invoice-payment') : navigate('/error');
+        }
+    }
+
+    return (
+        <>
+            {clientAlert ? (
+                <div className="clientAlertText">
+                    <ErrorOutlineIcon/>
+                    <p>Wybierz klienta z listy</p>
+                </div>
+            ) : null}
+            <Formik
+                initialValues={initialValues}
+                validationSchema={schemaAddInvoice}
+                onSubmit={(values) => {
+                    addInvoice(values)
+                }}
+            >
+                {({errors, touched}) => (
+                    <Form className='form-box'>
+                        <p className="field-description-text">Miejsce wystawienia</p>
+                        <Field
+                            name="place"
+                            type="string"
+                            as={StyledTextField}
+                            variant="filled"
+                            className="field-box"
+                            size="small"
+                            label="Podaj miejscowość wystawienia faktury"
+                            error={Boolean(errors.place) && Boolean(touched.place)}
+                            helperText={Boolean(touched.place) && errors.place}
+                        />
+                        <p className="field-description-text">Data wystawienie</p>
+                        <Field
+                            name="creationDate"
+                            type="date"
+                            as={StyledTextField}
+                            variant="filled"
+                            className="field-box"
+                            size="small"
+                            label="Podaj datę wystawienia faktury"
+                            error={Boolean(errors.creationDate) && Boolean(touched.creationDate)}
+                            helperText={Boolean(touched.creationDate) && errors.creationDate}
+                        />
+                        <p className="field-description-text">Data wykonania usługi</p>
+                        <Field
+                            name="dateOfService"
+                            type="date"
+                            as={StyledTextField}
+                            variant="filled"
+                            className="field-box"
+                            size="small"
+                            label="Podaj datę wykonania usługi"
+                            error={Boolean(errors.dateOfService) && Boolean(touched.dateOfService)}
+                            helperText={Boolean(touched.dateOfService) && errors.dateOfService}
+                        />
+                        <p className="field-description-text">Osoba wystawiająca fakturę</p>
+                        <Field
+                            name="personCreatingInvoice"
+                            type="string"
+                            as={StyledTextField}
+                            variant="filled"
+                            className="field-box"
+                            size="small"
+                            label="Podaj imię i nazwisko"
+                            error={Boolean(errors.personCreatingInvoice) && Boolean(touched.personCreatingInvoice)}
+                            helperText={Boolean(touched.personCreatingInvoice) && errors.personCreatingInvoice}
+                        />
+                        <ConfirmBtn text={props.buttonText} type='submit' />
+                    </Form>
+                )}
+            </Formik>
+        </>
+    )
+}
--- src/views/InvoiceAdd/InvoiceAdd.tsx
+++ src/views/InvoiceAdd/InvoiceAdd.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {Header} from "../../components/Header/Header";
 import {SecondHeader} from "../../components/SecondHeader/SecondHeader";
 import {Footer} from "../../components/Footer/Footer";
+import {InvoiceAddBox} from "../../components/InvoiceAddBox/InvoiceAddBox";
 
 export const InvoiceAdd = () => {
     return(
         <nav className="views">
             <Header mainHeaderText='Dodaj fakturę'/>
             <SecondHeader secondHeaderText='Utwórz nową fakturę'></SecondHeader>
-            <div>Formularz pierwszego etapu dodawania faktury.</div>
+            <InvoiceAddBox/>
             <Footer/>
         </nav>
     )
