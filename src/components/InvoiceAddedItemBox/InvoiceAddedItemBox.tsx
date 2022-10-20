9ade2c185d338fd3b711ba6e157c34aca1064756
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-20 19:41:00 +0200

    Created InvoiceAddedItem view.

---
+++ src/components/InvoiceAddedItemBox/InvoiceAddedItemBox.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {ConfirmBtn} from "../../common/ConfirmBtn/ConfirmBtn";
+import {useParams} from "react-router-dom";
+import '../ComponentsStyles.css'
+import {isMobile} from "react-device-detect";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+
+export const InvoiceAddedItemBox = () => {
+    let param = useParams();
+
+    return(
+        <div className={isMobile ? 'data-box-mobile' : 'data-box'}>
+            <CheckCircleOutlineIcon className='main-info-icon'/>
+            <p className='main-info-text'>Dodano pozycję do faktury</p>
+            <ConfirmBtn text='Dodaj kolejną pozycję' to={`/invoice-add-item/${param.invoiceId}`}/>
+            <ConfirmBtn text='Przejdź do podsumowania' to={`/invoice-summary/${param.invoiceId}`}/>
+        </div>
+    )
+}
---
+++ src/views/InvoiceAddedItem/InvoiceAddedItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Header} from "../../components/Header/Header";
+import {SecondHeader} from "../../components/SecondHeader/SecondHeader";
+import {Footer} from "../../components/Footer/Footer";
+import {InvoiceAddedItemBox} from "../../components/InvoiceAddedItemBox/InvoiceAddedItemBox";
+
+export const InvoiceAddedItem = () => {
+    return(
+        <nav className="views">
+            <Header mainHeaderText='Dodaj fakturę'/>
+            <SecondHeader secondHeaderText='Dodawanie pozycji do faktury'></SecondHeader>
+            <InvoiceAddedItemBox/>
+            <Footer/>
+        </nav>
+    )
+}
