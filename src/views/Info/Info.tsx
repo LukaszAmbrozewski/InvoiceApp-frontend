84212616300d8def98ffb7bf843cfcf86db30033
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-22 15:11:42 +0200

    Configured info view.

--- src/views/Info/Info.tsx
+++ src/views/Info/Info.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {Header} from "../../components/Header/Header";
 import {SecondHeader} from "../../components/SecondHeader/SecondHeader";
 import {Footer} from "../../components/Footer/Footer";
+import "../../components/ComponentsStyles.css";
+import {InfoBox} from "../../components/InfoBox/InfoBox";
 
 export const Info = () => {
     return(
         <nav className="views">
             <Header mainHeaderText='Informacje'/>
             <SecondHeader secondHeaderText='Informacje o aplikacji'></SecondHeader>
-            <div>Podstawowe dane o aplikacji autorze celu aplikacji linki linkedin...</div>
+            <InfoBox/>
             <Footer/>
         </nav>
     )
